fb5223e72cfe4a329a502e2ac5691801b3c59005
LukeJS <lukejsandler@gmail.com>
2016-08-14 18:26:11 +0100

    Use MessageChannels to prefix the chat instead of cancelling the chat event altogether

--- src/main/java/me/nentify/titles/events/PlayerEventHandler.java
+++ src/main/java/me/nentify/titles/events/PlayerEventHandler.java
@@ -4,40 +4,79 @@ import me.nentify.titles.Titles;
 import me.nentify.titles.player.TitlesPlayer;
 import me.nentify.titles.stats.Stat;
 import me.nentify.titles.titles.Title;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.message.MessageChannelEvent;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.text.channel.MessageReceiver;
+import org.spongepowered.api.text.chat.ChatType;
+import org.spongepowered.api.text.format.TextColors;
 
+import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.Optional;
 
 public class PlayerEventHandler {
+
+    private TitlesMessageChannel titlesChannel = new TitlesMessageChannel();
 
     @Listener(order = Order.LATE)
     public void onPlayerChat(MessageChannelEvent.Chat event, @First Player player) {
         Optional<TitlesPlayer> titlesPlayerOptional = Titles.getTitlesPlayer(player.getUniqueId());
 
+        // Chat stat
         if (titlesPlayerOptional.isPresent()) {
             TitlesPlayer titlesPlayer = titlesPlayerOptional.get();
 
-            // Chat stat
             titlesPlayer.incrementStat(Stat.CHAT_MESSAGES);
             titlesPlayer.checkTitle(Title.Type.CHATTY, player);
-
-            // Format chat with custom title
-            Text message = event.getMessage();
-
-            event.setCancelled(true);
-
-            Sponge.getGame().getServer().getOnlinePlayers().stream().filter(x -> !x.getUniqueId().equals(titlesPlayer.getUUID())).forEach(x -> x.sendMessage(getTitledMessage(titlesPlayer, message, false)));
-            player.sendMessage(getTitledMessage(titlesPlayer, message, true));
         }
+
+        // Prefix chat with title
+        MessageChannel originalChannel = event.getOriginalChannel();
+        MessageChannel newChannel = MessageChannel.combined(originalChannel, titlesChannel);
+        event.setChannel(newChannel);
     }
 
-    private Text getTitledMessage(TitlesPlayer titlesPlayer, Text message, boolean edit) {
-        return Text.builder().append(titlesPlayer.getCurrentTitle().getPrefixText(edit)).append(Text.of(" ")).append(message).build();
+    public class TitlesMessageChannel implements MessageChannel {
+
+        @Override
+        public Optional<Text> transformMessage(@Nullable Object sender, MessageReceiver recipient, Text original, ChatType type) {
+            Player senderPlayer = (Player) sender;
+            Player receiverPlayer = (Player) recipient;
+
+            Optional<TitlesPlayer> titlesPlayer = Titles.getTitlesPlayer(senderPlayer.getUniqueId());
+
+            if (titlesPlayer.isPresent()) {
+                return Optional.of(
+                        getTitledMessage(
+                                titlesPlayer.get(),
+                                original,
+                                senderPlayer.getUniqueId().equals(
+                                        receiverPlayer.getUniqueId()
+                                )
+                        )
+                );
+            }
+
+            return Optional.of(original);
+        }
+
+        @Override
+        public Collection<MessageReceiver> getMembers() {
+            return null;
+        }
+
+        private Text getTitledMessage(TitlesPlayer titlesPlayer, Text message, boolean edit) {
+            return Text.builder()
+                    .append(Text.of(TextColors.GRAY, "["))
+                    .append(titlesPlayer.getCurrentTitle().getPrefixText(edit))
+                    .append(Text.of(TextColors.GRAY, "] "))
+                    .append(message)
+                    .build();
+        }
     }
 }
